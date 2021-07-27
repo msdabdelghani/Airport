c4536aa97122f90efa65a6ee26c51a4bf1f1d3bc
Abdelghani Moussaid <moussaidabdelghani@gmail.com>
2021-07-27 02:02:43 +0100

    Adding the passenger's class in models

--- models.py
+++ models.py
@@ -17,3 +17,11 @@ class Flight(models.Model):
     duration = models.IntegerField()
     def __str__(self):
         return f"{self.origin} -> {self.destination} IN {self.duration}"
+
+class Passenger(models.Model):
+    first = models.CharField(max_length=100)
+    last = models.CharField(max_length=100)
+    flights = models.ManyToManyField(Flight, blank= True, related_name="passengers")
+
+    def __str__(self):
+        return f"{self.first} {self.last}"
