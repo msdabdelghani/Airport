99b06f0978178adb34322628cead619493529eff
Abdelghani Moussaid <moussaidabdelghani@gmail.com>
2021-07-27 02:03:25 +0100

    Adding the passenger's model for the admin

--- admin.py
+++ admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from flights.models import Flight, Airport
+from flights.models import Flight, Airport, Passenger
 
 # Register your models here.
 
 admin.site.register(Flight)
 admin.site.register(Airport)
+admin.site.register(Passenger)
